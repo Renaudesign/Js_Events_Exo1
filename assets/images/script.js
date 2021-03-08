228d43da199a4cd1164361456732ceadc7b857c8
renaudesign <renaud.sourmail@gmail.com>
2021-03-08 16:16:49 +0100

    JS Events exo 1 terminé

---
+++ assets/images/script.js
@@ -0,0 +1,7 @@
+function MouseRollover() {
+    Document.getElementById(image2).src = "assets/images/image1_2.jpg";
+    }
+function MouseOut() {
+    Document.getElementById(image2).src = "assets/images/image1.jpg";
+    }
+ 
